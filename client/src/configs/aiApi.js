da47f6498badcc72f284ff1e60467864e5dbaed2
singh-29-naina <nainakushvah29@gmail.com>
2026-07-10 20:40:00 +0530

    deploy

--- client/src/configs/aiApi.js
+++ client/src/configs/aiApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const aiApi = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "https://resume-builder-2-8dbm.onrender.com",
 });
 
 export default aiApi;
